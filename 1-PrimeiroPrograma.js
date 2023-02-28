a997dd565f77ff79a0a7a3a9e8180275185609f9
Udiel <udiel.inacio@outlook.com>
2023-02-28 09:04:55 -0300

    variaveis

--- 1-PrimeiroPrograma.js
+++ 1-PrimeiroPrograma.js
@@ -1,1 +1,11 @@
-console.log("Meu primeiro programa");
+console.log("Meu primeiro programa trabal hando com variaveis");
+
+const idade = 26;
+
+console.log(idade)
+console.log(idade+2*20)
+console.log(idade/2)
+console.log(idade-2)
+
+const idadeSomada = idade+2;
+console.log(idadeSomada)
